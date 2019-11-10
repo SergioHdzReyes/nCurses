352d84b7d9ce68c904efbf6505621fec50680bbc
Sergio Hernández Reyes <sergio.hdzreyes@gmail.com>
2019-11-10 18:44:22 +0000

    Se agrega prueba de color, se mejora makefile para una mejor compilacion

---
+++ Chapter 3/colortest.c
@@ -0,0 +1,34 @@
+#include <ncurses.h>
+#include <stdlib.h>
+
+void bomb(char *msg);
+
+int main(void) {
+  initscr();
+
+  /* first test for color ability fo the terminal */
+  if (!has_colors())
+    bomb("Terminal cannot do colors\n");
+
+  /* next attemp to initialize curses colors */
+  if (start_color() != OK)
+    bomb("Unable to start colors.\n");
+
+  /* colors are okay: continue */
+  printw("Colors have been properly initialized.\n");
+  printw("Congratulations!\n");
+  printw("NCurses reports that you can use %d colors, \n", COLORS);
+  printw("and %d color pairs.", COLOR_PAIRS);
+  refresh();
+  getch();
+  
+  endwin();
+  return 0;
+}
+
+void bomb(char *msg)
+{
+  endwin();
+  puts(msg);
+  exit(1);
+}
