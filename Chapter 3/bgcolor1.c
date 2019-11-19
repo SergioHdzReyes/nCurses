6e77ea8f6b1c82649839837c4a987785ab5a183a
Sergio Hernández Reyes <sergio.hdzreyes@gmail.com>
2019-11-19 03:45:06 +0000

    Se cambia color de fondo de terminal.

---
+++ Chapter 3/bgcolor1.c
@@ -0,0 +1,15 @@
+#include <ncurses.h>
+#include "../tools.h"
+
+int main(void)
+{
+  init_curses(true);
+
+  init_pair(1, COLOR_WHITE, COLOR_BLUE);
+  bkgd(COLOR_PAIR(1));
+  refresh();
+  getch();
+
+  endwin();
+  return 0;
+}
