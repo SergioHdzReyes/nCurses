86c373a4f63289f4073ef3204b8ff72f4c76f2a6
Sergio Hernandez Reyes <sergio.hdzreyes@gmail.com>
2020-07-13 23:09:09 -0500

    Se comienza a utilizar el movimiento de cursor en pantalla

---
+++ Chapter 4/corners1.c
@@ -0,0 +1,33 @@
+#include <ncurses.h>
+
+void main(void)
+{
+  int rows, cols;
+
+  initscr();
+  getmaxyx(stdscr, rows, cols);
+  rows--;
+  cols--;
+
+  move(0, 0);
+  addch('*');
+  refresh();
+  napms(500);
+
+  move(0, cols);
+  addch('*');
+  refresh();
+  napms(500);
+
+  move(rows, 0);
+  addch('*');
+  refresh();
+  napms(00);
+
+  move(rows, cols);
+  addch('*');
+  refresh();
+  getch();
+
+  endwin();
+}
