3ed83d2041aef7ebd57a517e34993b17fe195278
Sergio Hernandez Reyes <sergio.hdzreyes@gmail.com>
2020-09-16 17:47:01 -0500

    Se utilizan funciones para insertar o eliminar lineas o texto simple

---
+++ Chapter 5/hamlet1.c
@@ -0,0 +1,25 @@
+#include <ncurses.h>
+
+int main(void)
+{
+  int rows, cols;
+
+  initscr();
+
+  char Ham1[] = "To be, or not to be: that is the question:\n";
+  char Ham2[] = "Whether 'tis nobler in the mind to suffer\n";
+  char Ham3[] = "The slings and arrows of outrageous fortune, \n";
+  char Ham4[] = "Or to take arms agains a sea of troubles, \n";
+  char Ham5[] = "And by opposing end them?\n";
+
+  initscr();
+
+  addstr(Ham1);
+  addstr(Ham3);
+  addstr(Ham5);
+  refresh();
+  getch();
+
+  endwin();
+  return 0;
+}
---
+++ Chapter 5/hamlet2.c
@@ -0,0 +1,31 @@
+#include <ncurses.h>
+
+int main(void)
+{
+  int rows, cols;
+
+  initscr();
+
+  char Ham1[] = "To be, or not to be: that is the question:\n";
+  char Ham2[] = "Whether 'tis nobler in the mind to suffer\n";
+  char Ham3[] = "The slings and arrows of outrageous fortune, \n";
+  char Ham4[] = "Or to take arms agains a sea of troubles, \n";
+  char Ham5[] = "And by opposing end them?\n";
+
+  initscr();
+
+  addstr(Ham1);
+  addstr(Ham3);
+  addstr(Ham5);
+  refresh();
+  getch(); /* wait for key press */
+
+  move(1,0); /* position cursor */
+  insertln(); /* insert a blank line, scroll text down */
+  addstr(Ham2); /* line to insert */
+  refresh();
+  getch(); /* wait for key press */
+
+  endwin();
+  return 0;
+}
